0f08bf15cab637774751445cded36e82c4004efa
OtavioLira <=>
2024-03-12 10:34:36 -0300

    Juntando funções

--- funcoes_sentenca.py
+++ funcoes_sentenca.py
@@ -19,22 +19,31 @@ def identificar_nucle_predicado(frase):
     else:
         return "Predicado não encontrado"
 
+def identificar_predicado(frase):
+    verbos = ["correu", "andou"]
+    frase = frase.split()
+    final = len(frase)
+
+    for palavra in frase:    
+
+        if palavra in verbos:
+            verbo_inicio = frase.index(palavra)
+
+        if palavra == ",":
+            final = frase.index(palavra)
+            break
+
+    return frase[verbo_inicio:final]
+
 # Exemplo de uso
 frase = "O cachorro correu até o parque, e o gato andou até a praça."
+#frase = ["o", "cachorro", "correu", "até", "o", "parque"]
+
 predicados = identificar_nucle_predicado(frase)
+print("Os nucleos do predicado da frase são:", predicados)
+predicados = identificar_predicado(frase)
 print("Os predicados da frase são:", predicados)
 
-verbos = ["correu", "andou"]
-frase = ["o", "cachorro", "correu", "até", "o", "parque"]
-final = len(frase)
 
-for palavra in frase:    
 
-    if palavra in verbos:
-        verbo_inicio = frase.index(palavra)
 
-    if palavra == ",":
-        final = frase.index(palavra)
-        break
-
-print(frase[verbo_inicio:final])
